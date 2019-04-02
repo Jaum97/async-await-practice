77c839076f12be8b8b7ccb41b9effad2f01ddd77
ChilenoSika <jaum97@yahoo.com.br>
2019-04-02 00:46:27 -0300

    Async getRepos

--- practice-github-api/src/App.js
+++ practice-github-api/src/App.js
@@ -1,5 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const App = () => <h1>hello world</h1>;
+export default function App() {
+  const { log: l } = console;
 
-export default App;
+  const [repositories, setRepositories] = useState([]);
+
+  async function getRepos(user) {
+    const response = await fetch(`https://api.github.com/users/${user}/repos`);
+    return await response.json();
+  }
+
+  useEffect(() => {
+    getRepos("jaum97").then(response => setRepositories(response));
+  }, []);
+
+  return (
+    <ul>
+      {repositories.map(repo => (
+        <li key={repo.id}>
+          <strong>{repo.name}</strong>
+        </li>
+      ))}
+    </ul>
+  );
+}
